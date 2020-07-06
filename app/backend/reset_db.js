47e5ad87300a994532daa29cec32491d74725ff1
Matt Gowie <matt@masterpoint.io>
2020-07-05 18:57:27 -0600

    Adds reset_db npm run script

---
+++ app/backend/reset_db.js
@@ -0,0 +1,23 @@
+require("./config/config");
+
+const { Todo } = require("./models/todos/todo");
+const db = require("./db");
+
+const app = {
+  emit: function (msg) {
+    console.log("Emitted: ", msg)
+    Todo.deleteMany({}, (err, result) => {
+      if (err === null) {
+        console.log("Err: ", err, " Result ", result)
+        process.exit(1)
+      } else {
+        console.log("Successfully deleted all todos!")
+        process.exit(0)
+      }
+    })
+  }
+}
+
+db.connect(app);
+
+
